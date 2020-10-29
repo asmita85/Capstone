c0218ff7c43daa2b6220e57164dca935320e616c
Asma Charef <asma.charef@hotmail.com>
2020-10-29 13:55:49 -0500

    firrebase update

--- firebase/config.js
+++ firebase/config.js
@@ -1,10 +1,32 @@
+// export default {
+//   apiKey: "AIzaSyBJPWqzz6p0Y3a_nxM2L2krAaD4JpxH2dM",
+//   authDomain: "shop-data-7dbae.firebaseapp.com",
+//   databaseURL: "https://shop-data-7dbae.firebaseio.com",
+//   projectId: "shop-data-7dbae",
+//   storageBucket: "shop-data-7dbae.appspot.com",
+//   messagingSenderId: "190585088076",
+//   appId: "1:190585088076:web:a95da750a7d41b6fa9ace9",
+//   measurementId: "G-QSGX1E638G"
+// };
+// export default {
+//   apiKey: "AIzaSyDaup1y6DI1g5Mg1kKcyMmJ56m-3i3g56U",
+//   authDomain: "shopupdate-99257.firebaseapp.com",
+//   databaseURL: "https://shopupdate-99257.firebaseio.com",
+//   projectId: "shopupdate-99257",
+//   storageBucket: "shopupdate-99257.appspot.com",
+//   messagingSenderId: "736341129633",
+//   appId: "1:736341129633:web:2e86d1fc456fa616d4b3e0",
+//   measurementId: "G-95GEL2NT04"
+// };
 export default {
-  apiKey: "AIzaSyBJPWqzz6p0Y3a_nxM2L2krAaD4JpxH2dM",
-  authDomain: "shop-data-7dbae.firebaseapp.com",
-  databaseURL: "https://shop-data-7dbae.firebaseio.com",
-  projectId: "shop-data-7dbae",
-  storageBucket: "shop-data-7dbae.appspot.com",
-  messagingSenderId: "190585088076",
-  appId: "1:190585088076:web:a95da750a7d41b6fa9ace9",
-  measurementId: "G-QSGX1E638G"
-};
+  {
+    apiKey: "AIzaSyDCW7yuG8aX7jLxl08G0wLyRmGE4UG9uWQ",
+    authDomain: "capstone-3a9f6.firebaseapp.com",
+    databaseURL: "https://capstone-3a9f6.firebaseio.com",
+    projectId: "capstone-3a9f6",
+    storageBucket: "capstone-3a9f6.appspot.com",
+    messagingSenderId: "721859631758",
+    appId: "1:721859631758:web:2b939a646a8882ee4a07df",
+    measurementId: "G-X6VYX6LFRR"
+  };
+}
